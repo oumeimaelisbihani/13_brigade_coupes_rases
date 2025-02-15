74e7ff22d45c873f2c665c2eeca2b9e56d31213d
sbailleul <bailleul.sacha@gmail.com>
2025-02-15 02:55:45 +0100

    feat(offline): Add first implementation of pwa

--- frontend/src/shared/components/AppLayout.tsx
+++ frontend/src/shared/components/AppLayout.tsx
@@ -1,11 +1,4 @@
-import {
-	Dialog,
-	DialogBackdrop,
-	DialogPanel,
-	TransitionChild,
-} from "@headlessui/react";
-
-import { type ReactNode, useState } from "react";
+import type { ReactNode } from "react";
 import { Navbar } from "./Navbar";
 
 interface Props {
@@ -13,8 +6,6 @@ interface Props {
 	sideBar?: ReactNode;
 }
 export function AppLayout({ children, sideBar }: Props) {
-	const [sidebarOpen, setSidebarOpen] = useState(false);
-
 	return (
 		<>
 			<div className="h-screen">
@@ -27,39 +18,6 @@ export function AppLayout({ children, sideBar }: Props) {
 						</div>
 					)}
 				</div>
-				<Dialog
-					open={sidebarOpen}
-					onClose={setSidebarOpen}
-					className="relative z-50 lg:hidden"
-				>
-					<DialogBackdrop
-						transition
-						className="fixed inset-0 bg-gray-900/80 transition-opacity duration-300 ease-linear data-[closed]:opacity-0"
-					/>
-
-					<div className="fixed inset-0 flex">
-						<DialogPanel
-							transition
-							className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-[closed]:-translate-x-full"
-						>
-							<TransitionChild>
-								<div className="absolute left-full top-0 flex w-16 justify-center pt-5 duration-300 ease-in-out data-[closed]:opacity-0">
-									<button
-										type="button"
-										onClick={() => setSidebarOpen(false)}
-										className="-m-2.5 p-2.5"
-									>
-										<span className="sr-only">Close sidebar</span>
-									</button>
-								</div>
-							</TransitionChild>
-							{/* Sidebar component, swap this element with another sidebar if you like */}
-							<div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4">
-								<div className="flex flex-1 flex-col">{sideBar}</div>
-							</div>
-						</DialogPanel>
-					</div>
-				</Dialog>
 			</div>
 		</>
 	);
